40f3990d6de01c8fe279cd422243bac5f8d03f10
KevinChiu <fsd83811@gmail.com>
2020-09-30 17:16:09 +0800

    20200903_rfid_node

--- microsoft_rfid/src/rfid_py.py
+++ microsoft_rfid/src/rfid_py.py
@@ -12,7 +12,13 @@ import sys
 g_ser_rfid = serial.Serial('/dev/ttyUSB0', 38400, bytesize=8,
                            parity=serial.PARITY_EVEN, stopbits=1, timeout=0.07)
 
-
+def rfidFuc():
+    g_ser_rfid.write([0xaa, 0xdd, 0x00, 0x03, 0x01, 0x0c, 0x0d])
+    read = g_ser_rfid.read(18)
+    read_hex = read.encode('hex')
+    if(len(read_hex) == 18):
+        # print(read_hex)
+        return read_hex
 
 def rfid_talker():
     pub = rospy.Publisher('rfid_id', String, queue_size=10)
@@ -20,14 +26,7 @@ def rfid_talker():
     rate = rospy.Rate(10)  # 10hz
     while not rospy.is_shutdown():
 
-        g_ser_rfid.write([0xaa, 0xdd, 0x00, 0x03, 0x01, 0x0c, 0x0d])
-
-        read = g_ser_rfid.read(18)
-        read_hex = read.encode('hex')
-        if(len(read_hex) == 18):
-            print(read_hex)
-
-        rfid_id = "hello world %s" % rospy.get_time()
+        rfid_id = rfidFuc()
         rospy.loginfo(rfid_id)
         pub.publish(rfid_id)
         rate.sleep()
--- test/rfid_py2.py
+++ test/rfid_py2.py
@@ -15,18 +15,20 @@ g_ser_rfid = serial.Serial('/dev/ttyUSB0', 38400, bytesize=8,
                            parity=serial.PARITY_EVEN, stopbits=1, timeout=0.07)
 
 
-while 1:
-
+def rfidFuc():
     g_ser_rfid.write([0xaa, 0xdd, 0x00, 0x03, 0x01, 0x0c, 0x0d])
-
     read = g_ser_rfid.read(18)
     read_hex = read.encode('hex')
     if(len(read_hex) == 18):
-        print(read_hex)
+        # print(read_hex)
+        return read_hex
+
+while 1:
+    print(rfidFuc())
 
 
-class RfidFuc(object):
-    """
-    docstring
-    """
-    pass
+
+# class RfidClass():
+#     def __init__(self):
+#         self.data = []
+#     pass
